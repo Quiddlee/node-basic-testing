6b2cdd6e2071a3c7faeaa35960708a28c8b9e430
Bohdan <bogdanscherbinadev@gmail.com>
2024-01-28 14:31:48 +0200

    feat: implement mocking lib api tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,58 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+const RELATIVE_PATH = 'users';
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const FAKE_RESPONSE_DATA = { data: 'test' };
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockedCreate = jest.spyOn(axios, 'create');
+
+    await throttledGetDataFromApi(RELATIVE_PATH);
+
+    expect(mockedCreate).toHaveBeenCalledWith({
+      baseURL: BASE_URL,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const axiosClient = axios.create({
+      baseURL: BASE_URL,
+    });
+    const mockedAxiosClientGet = jest.spyOn(axiosClient, 'get');
+    const mockedCreate = jest.spyOn(axios, 'create');
+    mockedCreate.mockReturnValue(axiosClient);
+
+    await throttledGetDataFromApi(RELATIVE_PATH);
+
+    expect(mockedAxiosClientGet).not.toHaveBeenCalled();
+
+    jest.runAllTimers();
+
+    expect(mockedAxiosClientGet).toHaveBeenCalledWith(RELATIVE_PATH);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const axiosClient = axios.create({
+      baseURL: BASE_URL,
+    });
+    const mockedAxiosClientGet = jest.spyOn(axiosClient, 'get');
+    const mockedCreate = jest.spyOn(axios, 'create');
+    mockedCreate.mockReturnValue(axiosClient);
+    mockedAxiosClientGet.mockReturnValue(Promise.resolve(FAKE_RESPONSE_DATA));
+
+    expect(await throttledGetDataFromApi(RELATIVE_PATH)).toBe(
+      FAKE_RESPONSE_DATA.data,
+    );
+    expect(mockedAxiosClientGet).toHaveBeenCalled();
   });
 });
